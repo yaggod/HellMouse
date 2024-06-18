de20fb683cb73d072087b4102b698b5377a492d6
yaggod <yaggod@yandex.ru>
2024-06-18 22:51:33 +0300

    Add tray icon

--- HellMouse/Program.cs
+++ HellMouse/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace HellMouse
+﻿
+namespace HellMouse
 {
 	internal class Program
 	{
+		[STAThread]
 		static void Main(string[] args)
 		{
 			new HellMouse(100, 500).Begin();
+
+			SetupTrayIcon();
+		}
+
+		private static void SetupTrayIcon()
+		{
+			TrayIconContainer form = new TrayIconContainer();
+			Application.Run(form);
 		}
 	}
 }
---
+++ HellMouse/TrayIconContainer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellMouse
+{
+	public class TrayIconContainer : Form
+	{
+		public NotifyIcon _trayIcon;
+
+		public TrayIconContainer() : base()
+		{
+			InitializeComponent();
+		}
+
+
+
+		protected override void OnLoad(EventArgs e)
+		{
+			Visible = false;
+			ShowInTaskbar = false;
+			base.OnLoad(e);
+		}
+
+
+		private void InitializeComponent()
+		{
+			var components = new System.ComponentModel.Container();
+			_trayIcon = new NotifyIcon(components);
+			SuspendLayout();
+			_trayIcon.Icon = SystemIcons.Application;
+			_trayIcon.Text = "HellMouse";
+			_trayIcon.Visible = true;
+			_trayIcon.ContextMenuStrip = new();
+			_trayIcon.ContextMenuStrip.Items.Add("Exit app", null, (sender, eventArgs) => Application.Exit());
+			ResumeLayout(false);
+		}
+	}
+}
