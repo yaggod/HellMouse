54068cf03f1d798bc5df05109239daeb577760bb
yaggod <yaggod@yandex.ru>
2024-06-18 22:06:59 +0300

    create class with useful for enums method

---
+++ HellMouse/EnumExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace HellMouse
+{
+	public static class EnumExtension<T> where T : struct, Enum
+	{
+		private static T[] _items;
+		private static Random _random = new();
+		static EnumExtension()
+		{
+			_items = Enum.GetValues<T>();
+		}
+
+		public static T GetRandomItem()
+		{
+			 return _items[_random.Next(_items.Length)];
+		}
+	}
+}
