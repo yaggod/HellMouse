e44fffa3a02d5b1c96d3d15baa948baf94d0ddbe
yaggod <yaggod@yandex.ru>
2024-06-18 22:07:15 +0300

    Implemented main logics

---
+++ HellMouse/HellMouse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellMouse
+{
+	internal class HellMouse
+	{
+		public int MinDelay { get; private set; }
+		public int MaxDelay { get; private set; }
+
+		private Thread _executionThread;
+
+
+		public HellMouse(int minDelay, int maxDelay)
+		{
+			MinDelay = minDelay;
+			MaxDelay = maxDelay;
+			_executionThread = new Thread(Workflow);
+		}
+
+		public void Begin()
+		{
+			_executionThread.Start();
+		}
+
+		public void Stop()
+		{
+
+		}
+
+		private void Workflow()
+		{
+			Random random = new();
+			for(int i = 0; i < 5; i++)
+			//while(true)
+			{
+				PerformAction();
+				Task.Delay(random.Next(MinDelay, MaxDelay)).Wait();
+            }
+        }
+
+		private static void PerformAction()
+		{
+			Random random = new();
+			MouseEvent mouseEvent = EnumExtension<MouseEvent>.GetRandomItem();
+			int x = random.Next(0, 1920);
+			int y = random.Next(0, 1080);
+
+			Console.WriteLine($"{mouseEvent} {x} {y}");
+			WinApi.SetCursorPos(x, y);
+            WinApi.mouse_event((uint) mouseEvent);
+		}
+	}
+}
--- HellMouse/Program.cs
+++ HellMouse/Program.cs
@@ -4,7 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			new HellMouse(100, 500).Begin();
 		}
 	}
 }
