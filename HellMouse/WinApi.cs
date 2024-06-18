30fbc4a751b2211daec689c15c82027dd17cdf5f
yaggod <yaggod@yandex.ru>
2024-06-18 21:36:14 +0300

    Import user32.dll functions to control mouse

---
+++ HellMouse/WinApi.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HellMouse
+{
+	public class WinApi
+	{
+		[DllImport("User32.Dll")]
+		public static extern long SetCursorPos(int x, int y);
+
+		[DllImport("user32.dll")]
+		public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons = 0, uint dwExtraInfo =0);
+
+
+	}
+
+
+	public enum MouseEvents
+	{
+		NONE = 0x00,
+		MOUSEEVENTF_LEFTDOWN = 0x02,
+		MOUSEEVENTF_LEFTUP = 0x04,
+		MOUSEEVENTF_RIGHTDOWN = 0x08,
+		MOUSEEVENTF_RIGHTUP = 0x10,
+		MOUSEEVENTF_MIDDLEDOWN = 0x20,
+		MOUSEEVENTF_MIDDLEUP = 0x40,
+	}
+}
